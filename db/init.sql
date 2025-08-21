154a4162c136706e4ef80a3cdc4968316375165e
varubogu <toyosuke@varubogu.com>
2025-08-22 07:28:19 +0900

    chore: DBソースを登録

--- db/init.sql
+++ db/init.sql
@@ -1,2 +1,4 @@
-CREATE USER gbf_bot_user WITH PASSWORD 'gbf_bot_password';
-CREATE DATABASE gbf_bot_database WITH OWNER gbf_bot_user;
+CREATE
+USER gbf_bot_user WITH PASSWORD 'gbf_bot_password';
+CREATE
+DATABASE gbf_bot_database WITH OWNER gbf_bot_user;
