f537fde821714057f196866754c3ee3584931862
varubogu <toyosuke@varubogu.com>
2025-09-24 19:44:27 +0000

    devcontainer整備中

--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,3 @@
-CREATE
-USER gbf_bot_user;
-CREATE
-DATABASE gbf_bot_db WITH OWNER gbf_bot_user;
-ALTER
-USER gbf_bot_user WITH PASSWORD 'gbf_bot_password';
+CREATE USER gbf_bot_user;
+CREATE DATABASE gbf_bot_db WITH OWNER gbf_bot_user;
+ALTER USER gbf_bot_user WITH PASSWORD 'gbf_bot_password';
