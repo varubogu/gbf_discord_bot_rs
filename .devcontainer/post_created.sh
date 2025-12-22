2e200907dcc6cd112bbd0534d63c90e54ff9b955
varubogu <toyosuke@varubogu.com>
2025-12-22 05:00:29 +0000

    chore: devcontainer構成の見直し

--- .devcontainer/post_created.sh
+++ .devcontainer/post_created.sh
@@ -5,7 +5,7 @@ rustc --version
 
 echo "### Updating package list..."
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 
 echo "### Installing PostgreSQL client..."
 sudo apt-get install -y postgresql-client
