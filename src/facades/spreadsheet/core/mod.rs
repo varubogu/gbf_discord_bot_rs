4887116c014f95009026313eb369d88e0d0a0db5
varubogu <toyosuke@varubogu.com>
2025-12-26 08:25:08 +0000

    refactor: スプレッドシートの処理のうち、子ファサードをcoreとしてフォルダ分け

---
+++ src/facades/spreadsheet/core/mod.rs
@@ -0,0 +1,5 @@
+pub mod export_facade;
+pub mod import_facade;
+
+pub use export_facade::{ExportResult, SpreadsheetExportFacade};
+pub use import_facade::{ImportResult, SpreadsheetImportFacade};
--- src/facades/spreadsheet/mod.rs
+++ src/facades/spreadsheet/mod.rs
@@ -1,11 +1,11 @@
+pub mod core;
 pub mod global_load_facade;
 pub mod global_push_facade;
 pub mod guild_load_facade;
 pub mod guild_push_facade;
 pub mod guild_spreadsheet_registration_facade;
-pub mod spreadsheet_export_facade;
-pub mod spreadsheet_import_facade;
 
+pub use core::{ExportResult, ImportResult, SpreadsheetExportFacade, SpreadsheetImportFacade};
 pub use global_load_facade::execute_global_load;
 pub use global_push_facade::execute_global_push;
 pub use guild_load_facade::execute_load;
@@ -13,5 +13,3 @@ pub use guild_push_facade::execute_push;
 pub use guild_spreadsheet_registration_facade::{
     GuildSpreadsheetRegistrationFacade, RegistrationResult,
 };
-pub use spreadsheet_export_facade::{ExportResult, SpreadsheetExportFacade};
-pub use spreadsheet_import_facade::{ImportResult, SpreadsheetImportFacade};
