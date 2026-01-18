ce00fab5472c0793321fd7051307b8af001644de
varubogu <toyosuke@varubogu.com>
2026-01-18 00:27:10 +0000

    chore: SQLサンプル更新

--- db/sql/crud_example.sql
+++ db/sql/crud_example.sql
@@ -6,4 +6,18 @@ select * from worker.notifications where schedule_datetime >= now() order by sch
 select * from worker.scheduled_tasks st
     left join worker.notifications n on st.id = n.task_id
 where task_type = 1
-order by st.schedule_datetime
+order by st.schedule_datetime;
+
+-- ギルドチャンネル一覧
+select * from guild_master.guild_channels gc
+    left join guild_master.guilds g on gc.guild_id = g.guild_id
+order by gc.guild_id, gc.channel_type;
+
+-- ギルドチャンネル1件更新
+update guild_master.guild_channels set
+  channel_id = :channel_id
+where guild_id = :guild_id and channel_type = :channel_type;
+
+-- ギルドチャンネル1件削除
+delete from guild_master.guild_channels
+where guild_id = :guild_id and channel_type = :channel_type;
