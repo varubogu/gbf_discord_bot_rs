07ecec1d98319ce2932ddac707fa426a1b63b3ac
varubogu <toyosuke@varubogu.com>
2026-02-18 16:17:07 +0000

    refactor: 後始末

--- src/infrastructure/database/db_helper.rs
+++
@@ -1,2 +0,0 @@
-/// 後方互換性のための再エクスポート
-pub use crate::infrastructure::database::session::set_current_guild_id;
--- src/infrastructure/database/mod.rs
+++ src/infrastructure/database/mod.rs
@@ -1,5 +1,4 @@
 pub mod connection;
 pub mod container;
-pub mod db_helper;
 pub mod repositories;
 pub mod session;
