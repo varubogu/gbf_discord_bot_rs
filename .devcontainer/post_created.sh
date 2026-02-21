595b21eab84ad145821677749bb584bda474d069
varubogu <toyosuke@varubogu.com>
2026-02-21 04:10:42 +0000

    chore: messages.ymlをソートするためのyq（Go版）をインストール

--- .devcontainer/post_created.sh
+++ .devcontainer/post_created.sh
@@ -13,4 +13,7 @@ sudo apt-get install -y postgresql-client
 echo "### Installing sea-orm-cli..."
 cargo install sea-orm-cli@^2.0.0-rc
 
+sudo wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/local/bin/yq
+sudo chmod +x /usr/local/bin/yq
+
 echo "### Setup complete!"
